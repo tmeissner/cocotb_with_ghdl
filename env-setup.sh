1ebfafd654772b5f4f1a3b54d17c9ff01628a621
tmeissner <programming@goodcleanfun.de>
2022-02-08 15:39:46 +0100

    Switch to submodule repos instead of local clones

--- env-setup.sh
+++ env-setup.sh
@@ -5,11 +5,3 @@ if [ ! -d cocotbext-wishbone/.git ]; then
   cd cocotbext-wishbone && patch < ../setup.py.patch
   cd ..
 fi
-
-if [ ! -d libvhdl/.git ]; then
-  git clone https://github.com/tmeissner/libvhdl.git
-fi
-
-if [ ! -d cryptocores/.git ]; then
-  git clone https://github.com/tmeissner/cryptocores.git
-fi
