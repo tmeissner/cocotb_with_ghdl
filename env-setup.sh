201ac4c6c2658e1a6f8b951b78e9ffd2529eb538
tmeissner <programming@goodcleanfun.de>
2022-03-10 17:00:47 +0100

    Use pypi version since wallento/cocotbext-wishbone#22 was fixed

--- env-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ ! -d cocotbext-wishbone/.git ]; then
-  git clone https://github.com/wallento/cocotbext-wishbone.git
-  cd cocotbext-wishbone && patch < ../setup.py.patch
-  cd ..
-fi
